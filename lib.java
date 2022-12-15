789e23f204ca9620206b7934658afba4073d9506
NatalyaGorbonosova <gorbonosova.n@mail.ru>
2022-12-16 02:52:34 +0300

    Task 3

--- lib.java
+++ lib.java
@@ -30,4 +30,9 @@ public class lib {
         System.out.print(arr[arr.length-1]);
     }
     
+    public static void printArraeString(String [] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
